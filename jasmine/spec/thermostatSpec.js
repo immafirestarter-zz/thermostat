3d7abd00d06fd040bd46f21a8f88459972addd08
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-10 15:53:13 +0100

    adds exceptions to control temp setting

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -7,9 +7,32 @@ describe('Thermostat', function(){
     it('thermostat starts at 20 degrees', function(){
       expect(thermostat.temprature).toEqual(20)
     });
+
     it('can increase temprature', function(){
        thermostat.increaseTemp(5);
-         expect(thermostat.temprature).toEqual(25); 
+
+         expect(thermostat.temprature).toEqual(25);
     });
+    it('can decrease temprature', function(){
+      thermostat.decreaseTemp(5);
+      expect(thermostat.temprature).toEqual(15)
+    });
+  });
+
+  describe('powerSave', function(){
+
+    it('limits temprature to 25 when on', function(){
+      expect(function(){
+        thermostat.increaseTemp(6);
+      }).toThrow(new Error("Max temp is 25 while powerSave is on"));
+    });
+
+    it('limits temprature to 32 when off', function(){
+      expect(function(){
+        thermostat.powerSave = 'off'
+        thermostat.increaseTemp(14);
+      }).toThrow(new Error("Max temp is 32 while powerSave is off"));
+    });
+
   });
 });
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,7 +1,21 @@
 function Thermostat(){
   this.temprature = 20
+  this.powerSave = 'on'
 };
 
 Thermostat.prototype.increaseTemp = function(temp){
+  powerCheck.apply(this, [this.powerSave, temp])
   this.temprature = this.temprature + temp
+};
+
+Thermostat.prototype.decreaseTemp = function(temp){
+  this.temprature = this.temprature - temp;
+};
+
+ function powerCheck(powerSave,temp){
+  if (this.temprature + temp > 25 && this.powerSave === 'on') {
+   throw new Error('Max temp is 25 while powerSave is on')
+ } else if (this.temprature + temp > 32 && this.powerSave ==='off'){
+   throw new Error('Max temp is 32 while powerSave is off')
+ };
 };
